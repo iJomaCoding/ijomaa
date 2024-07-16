719ddb2a3dd6861faadcc6135144ae986a65f071
iJomaCoding <ijomacoding@gmail.com>
2024-07-16 15:21:44 +0800

    Changes in styles

--- src/components/Admin/Login/AdminLogin.jsx
+++ src/components/Admin/Login/AdminLogin.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import styles from "./AdminLogin.module.css";
 import { Link } from "react-router-dom";
 import ijoma2_dark from "../../../assets/logo/ijoma2_dark.png";
+import ijomaLight from "../../../assets/logo/ijoma2_light.png";
 
 const AdminLogin = () => {
   return (
     <div className={styles.container}>
+      <Link to="/">
+        <img className={styles.containerLogo} src={ijomaLight} alt="iJoma" />
+      </Link>
+
       <div className={styles.loginContainer}>
-        <img src={ijoma2_dark} alt="" />
-        <h1>Admin user Login</h1>
+        <p>Admin</p>
+        <img src={ijoma2_dark} alt="iJoma" />
         <form>
           <div className={styles.infield}>
             <input type="email" placeholder="Email" required />
