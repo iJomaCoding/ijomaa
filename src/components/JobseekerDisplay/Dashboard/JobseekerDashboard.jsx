ca33f62e3ab07b733dc01aa606b5a0883774869b
iJomaCoding <ijomacoding@gmail.com>
2024-07-16 15:22:32 +0800

    Added  some headers and styles

--- src/components/JobseekerDisplay/Dashboard/JobseekerDashboard.jsx
+++ src/components/JobseekerDisplay/Dashboard/JobseekerDashboard.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import styles from "./JobseekerDashboard.module.css";
+import Header from "./Header";
 
 const JobseekerDashboard = () => {
   return (
     <div>
+      <Header />
       <h1>Jobseeker DASHBOARD</h1>
     </div>
   );
