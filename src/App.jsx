2958c1fbd289df67d1731db645799b022fe52b77
iJomaCoding <ijomacoding@gmail.com>
2024-08-19 13:07:41 +0800

    gn update ya path sang employee components

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import JobseekerDashboard from "./components/JobseekerDisplay/Dashboard/Jobseeke
 import EmployerDashboard from "./components/EmployerDisplay/Dashboard/EmployerDashboard";
 import AdminDashboard from "./components/Admin/Dashboard/AdminDashboard";
 import LandingPage from "./components/Hero/LandingPage";
+import EmProfile from "./components/EmployerDisplay/Profile/Profile";
+import EmApplications from "./components/EmployerDisplay/Applications/Applications";
 
 function App() {
   return (
@@ -15,16 +17,21 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />}></Route>
         <Route path="/login" element={<SlideLogin />}></Route>
+        {/* jobseeker */}
         <Route path="/jobseeker" element={<JobseekerReg />}></Route>
         <Route
           path="/jobseeker/dashboard/*"
           element={<JobseekerDashboard />}
         ></Route>
+        {/* employer */}
         <Route path="/employer" element={<EmployerReg />}></Route>
         <Route
           path="/employer/dashboard/*"
           element={<EmployerDashboard />}
         ></Route>
+        <Route path="/employer/profile" element={<EmProfile />} />
+        <Route path="/employer/application" element={<EmApplications />} />
+        {/* admin */}
         <Route path="/admin/login" element={<AdminLogin />}></Route>
         <Route path="/admin/dashboard" element={<AdminDashboard />}></Route>
       </Routes>
