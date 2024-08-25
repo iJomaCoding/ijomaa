754833828d72a10fb3c7ae5f437076dd9b02948e
iJomaCoding <ijomacoding@gmail.com>
2024-08-25 20:20:31 +0800

    sidebar updates on jobseeker

--- src/components/JobseekerDisplay/Dashboard/Sidebar.jsx
+++ src/components/JobseekerDisplay/Dashboard/Sidebar.jsx
@@ -22,25 +22,31 @@ const Sidebar = ({ isOpen, toggleSidebar }) => {
   };
 
   return (
-    <div className={`${styles.sidebar} ${isOpen ? styles.open : ""}`}>
-      <button className={styles.closeBtn} onClick={toggleSidebar}>
-        <img src={backIcon} alt="Close" className={styles.backIcon} />
-      </button>
-      <div className={styles.sidebarContent}>
-        <Link to="home">
-          <div className={styles.tileBar}>Jobs</div>
-        </Link>
-        <Link to="profile">
-          <div className={styles.tileBar}>Profile</div>
-        </Link>
-        <Link to="applications">
-          <div className={styles.tileBar}>Applications</div>
-        </Link>
-        <Link to="/" onClick={handleLogout}>
-          <div className={styles.tileBar}>Log out</div>
-        </Link>
+    <>
+      <div
+        className={`${styles.overlay} ${isOpen ? styles.show : ""}`}
+        onClick={toggleSidebar}
+      />
+      <div className={`${styles.sidebar} ${isOpen ? styles.open : ""}`}>
+        <button className={styles.closeBtn} onClick={toggleSidebar}>
+          <img src={backIcon} alt="Close" className={styles.backIcon} />
+        </button>
+        <div className={styles.sidebarContent}>
+          <Link to="home">
+            <div className={styles.tileBar}>Jobs</div>
+          </Link>
+          <Link to="profile">
+            <div className={styles.tileBar}>Profile</div>
+          </Link>
+          <Link to="applications">
+            <div className={styles.tileBar}>Applications</div>
+          </Link>
+          <Link to="/" onClick={handleLogout}>
+            <div className={styles.tileBar}>Log out</div>
+          </Link>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
