d338ee4034f521dd6f05295f0776b9a55c15f8d7
iJomaCoding <ijomacoding@gmail.com>
2024-08-25 20:19:34 +0800

    updates on employee

--- src/components/EmployerDisplay/Profile/Profile.jsx
+++ src/components/EmployerDisplay/Profile/Profile.jsx
@@ -1,11 +1,53 @@
 import React from "react";
 import Header from "../Dashboard/Header";
+import styles from "./Profile.module.css";
 
 const Profile = () => {
   return (
     <div>
       <Header />
-      <div>porfile sections</div>
+      <main className={styles.mainContainer}>
+        <div>Profile section</div>
+        <section>
+          <div className={styles.inputField}>
+            <label htmlFor="">Company Name:</label>
+            <input type="text" placeholder="Enter your company name" />
+          </div>
+          <div className={styles.inputField}>
+            <label htmlFor="">First Name:</label>
+            <input type="text" placeholder="Enter your first name" />
+          </div>
+          <div className={styles.inputField}>
+            <label htmlFor="">Middle Name:</label>
+            <input type="text" placeholder="Enter your middle name" />
+          </div>
+          <div className={styles.inputField}>
+            <label htmlFor="">Last Name:</label>
+            <input type="text" placeholder="Enter your last name" />
+          </div>
+          <div className={styles.inputField}>
+            <label htmlFor="">Barangay:</label>
+            <input type="text" placeholder="Enter your barangay" />
+          </div>
+          <div className={styles.inputField}>
+            <label htmlFor="">Province:</label>
+            <input type="text" placeholder="Enter your province" />
+          </div>
+          <div className={styles.inputField}>
+            <label htmlFor="">City:</label>
+            <input type="text" placeholder="Enter your City" />
+          </div>
+          <div className={styles.inputField}>
+            <label htmlFor="">Postal Code:</label>
+            <input type="text" placeholder="Enter your postal code" />
+          </div>
+          <div className={styles.inputField}>
+            <label htmlFor="">Contact Number:</label>
+            <input type="text" placeholder="Enter your contact" />
+          </div>
+        </section>
+        <button type="submit">Save</button>
+      </main>
     </div>
   );
 };
